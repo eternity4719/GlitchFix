d679aade8ffa24df2aff59dddf06506b94867821
albert4719 <mcdoeswhat@qq.com>
2026-06-01 20:50:23 +0800

    1

--- src/main/kotlin/me/albert/glitchfix/fixes/ItemDrop.kt
+++ src/main/kotlin/me/albert/glitchfix/fixes/ItemDrop.kt
@@ -3,41 +3,9 @@ package me.albert.glitchfix.fixes
 import me.albert.corelib.utils.air
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.event.entity.ItemSpawnEvent
-import org.bukkit.event.player.PlayerDropItemEvent
 import org.bukkit.event.player.PlayerQuitEvent
-import org.bukkit.inventory.ItemStack
 
 object ItemDrop : Listener {
-
-    @EventHandler(ignoreCancelled = true)
-    fun onDrop(e: PlayerDropItemEvent) {
-        val player = e.player
-        if (player.isSneaking) {
-            return
-        }
-        val item = e.itemDrop.itemStack.clone()
-
-        if (player.inventory.firstEmpty() == -1) {
-            return
-        }
-
-        if (item.isPrecious()) {
-            e.isCancelled = true
-            player.sendMessage("§7[§b系统§7] §a检测到当前物品过于贵重,请按Shift+Q进行丢弃!")
-        }
-    }
-
-    @EventHandler(ignoreCancelled = true)
-    fun onItemSpawn(event: ItemSpawnEvent) {
-        val item = event.entity
-        val itemStack = item.itemStack
-        if (itemStack.itemMeta?.hasDisplayName() == false) {
-            return
-        }
-        item.customName(itemStack.effectiveName())
-        item.isCustomNameVisible = true
-    }
 
     @EventHandler
     fun onLeave(event: PlayerQuitEvent) {
@@ -55,22 +23,4 @@ object ItemDrop : Listener {
         inv.addItem(cursor)
     }
 
-    fun ItemStack.isPrecious(): Boolean {
-        // 使用 ?. 避免 ItemMeta 为空时崩溃，若为空则直接返回 false
-        val item = this
-        val meta = item.itemMeta ?: return false
-
-        // 检查 Lore、无法破坏属性以及是否包含 "BOX" (如 Shulker Box)
-        if (meta.hasLore() || meta.isUnbreakable || item.type.name.contains("BOX")) {
-            return true
-        }
-
-        val enchants = item.enchantments
-        if (enchants.size > 5) {
-            return true
-        }
-
-        // 检查是否有任何附魔等级大于 3
-        return enchants.values.any { it >= 3 }
-    }
 }
--- src/main/kotlin/me/albert/glitchfix/fixes/McMMO.kt
+++ src/main/kotlin/me/albert/glitchfix/fixes/McMMO.kt
@@ -1,11 +1,8 @@
 package me.albert.glitchfix.fixes
 
-import com.gmail.nossr50.events.skills.salvage.McMMOPlayerSalvageCheckEvent
 import io.papermc.paper.event.player.AsyncChatEvent
 import me.albert.corelib.utils.hasPD
 import me.albert.corelib.utils.isNull
-import me.albert.corelib.utils.sendMsg
-import me.albert.glitchfix.fixes.ItemDrop.isPrecious
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -13,15 +10,6 @@ import org.bukkit.inventory.ItemStack
 
 
 object McMMO : Listener {
-
-    @EventHandler(ignoreCancelled = true)
-    fun onSalvage(event: McMMOPlayerSalvageCheckEvent) {
-        val player: Player = event.getPlayer()
-        if (event.salvageItem.isPrecious() && !player.isSneaking) {
-            event.isCancelled = true
-            player.sendMsg("§b检测到当前物品过于珍贵,请按下Shift重新进行分解！")
-        }
-    }
 
     @EventHandler(ignoreCancelled = true)
     fun onChat(event: AsyncChatEvent) {
